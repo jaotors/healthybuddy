d378926739a0f56e4af22323b768bd7608903ac8
Joshua B. Turingan <jotu@splitmedialabs.com>
2020-08-17 16:30:26 +0800

    update: fix error not having ./app/App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-import App from './app/App'
-import { BrowserRouter } from 'react-router-dom'
-import { Grommet } from 'grommet'
-import React from 'react'
-import ReactDOM from 'react-dom'
+import App from './components/app';
+import { BrowserRouter } from 'react-router-dom';
+import { Grommet } from 'grommet';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
