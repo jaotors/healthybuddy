918c8f2f33f19a9a142531f0873510d1a0395e53
Joshua B. Turingan <jotu@splitmedialabs.com>
2020-08-17 22:36:24 +0800

    setup api

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import { Request } from './client';
+
+export const login = async (params) => {
+  const data = await Request.post('/login', { params });
+  return data;
+};
+
+export const register = async (params) => {
+  const data = await Request.post('/register', { params });
+  return data;
+};
+
+export const getUser = async (token) => {
+  const data = await Request.get('/user', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return data;
+};
+
+export const getAllProjects = async () => {
+  const data = Request.get('/projects');
+  return data;
+};
+
+export const getProject = async (projectId) => {
+  const data = Request.get(`/projects/${projectId}`);
+  return data;
+};
