509d908188dfcdeb3fe75dc2ad36f2d968cde520
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-08-20 12:19:39 +0800

    Display selected recipe on form

--- src/pages/dietitian/createMealPlan/createMealPlan.js
+++ src/pages/dietitian/createMealPlan/createMealPlan.js
@@ -14,12 +14,12 @@ import {
 import { CaretNext, CaretPrevious } from 'grommet-icons'
 import React, { useEffect, useState } from 'react'
 
+import { calculateTotalCalories } from '../../../utils/calorieCalc'
+import { foodData } from '../../../utils/foodData'
+
 const day = ['brealfast', 'lunch', 'dinner']
 
-const defaultOptions = []
-for (let i = 1; i <= 200; i += 1) {
-  defaultOptions.push(`option ${i}`)
-}
+const defaultOptions = foodData.map(food => food.recipe)
 
 const CreateMealPlan = () => {
   const [startDate, setstartDate] = useState(null)
@@ -32,9 +32,20 @@ const CreateMealPlan = () => {
   const onOpen = () => setOpen(true)
 
   const onClose = () => setOpen(undefined)
-
+  const onSubmit = () => {
+    onClose()
+  }
   const [options, setOptions] = useState(defaultOptions)
   const [value, setValue] = useState('')
+  const [selectedRecipe, setSelectedRecipe] = useState(null)
+  const [serving, setServing] = useState(0)
+
+  useEffect(() => {
+    const recipe = foodData.find(ele => {
+      return ele.recipe === value
+    })
+    setSelectedRecipe(recipe)
+  }, [value])
 
   useEffect(() => {
     const newMeals = [
@@ -71,7 +82,7 @@ const CreateMealPlan = () => {
 
   return (
     <>
-      {console.log(meals)}
+      {console.log(selectedRecipe)}
       <Box direction='row' justify='center'>
         <CaretPrevious color='brand' size='large' />
         <Box border width='large' justify='center' align='center'>
@@ -161,10 +172,13 @@ const CreateMealPlan = () => {
                     }}
                   />
                 </FormField>
-                <FormField label='grams'>
-                  <TextInput />
+                <FormField label='Grams'>
+                  <TextInput
+                    value={serving}
+                    onChange={event => setServing(event.target.value)}
+                  />
                 </FormField>
-                <FormField label='Second'>
+                <FormField label='Meal Time'>
                   <Select
                     options={['breakfast', 'lunch', 'dinner']}
                     value={select}
@@ -172,12 +186,48 @@ const CreateMealPlan = () => {
                     onChange={({ option }) => setSelect(option)}
                   />
                 </FormField>
+                <FormField label='calories'>
+                  <TextInput
+                    disabled
+                    value={
+                      selectedRecipe &&
+                      `${calculateTotalCalories(selectedRecipe, serving)} kcal`
+                    }
+                  />
+                </FormField>
+                <FormField label='fat'>
+                  <TextInput
+                    disabled
+                    value={
+                      selectedRecipe &&
+                      `${(selectedRecipe.fat * serving) / 10} grams`
+                    }
+                  />
+                </FormField>
+                <FormField label='protein'>
+                  <TextInput
+                    disabled
+                    value={
+                      selectedRecipe &&
+                      `${(selectedRecipe.protein * serving) / 10} grams`
+                    }
+                  />
+                </FormField>
+                <FormField label='carbohydrates'>
+                  <TextInput
+                    disabled
+                    value={
+                      selectedRecipe &&
+                      `${(selectedRecipe.carb * serving) / 10} grams`
+                    }
+                  />
+                </FormField>
               </Box>
               <Box flex={false} as='footer' align='start'>
                 <Button
                   type='submit'
                   label='Submit'
-                  onClick={onClose}
+                  onClick={onSubmit}
                   primary
                 />
               </Box>
